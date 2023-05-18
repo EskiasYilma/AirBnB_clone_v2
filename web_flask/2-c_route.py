f98fd93c07c3aa396f5da243318a29338129d65d
exuszee <exuszee@gmail.com>
2023-05-18 16:57:10 +0300

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """
+    Route to display “Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def display_HBNB():
+    """
+    Route to display “HBNB!”
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def display_c(text):
+    """
+    Route to display “C ” followed by the value of the text
+    """
+    result = "C " + text.replace("_", " ")
+    return result
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
