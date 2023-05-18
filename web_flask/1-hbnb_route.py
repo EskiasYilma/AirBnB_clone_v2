385e687f7b40df12746a62575ad681afebb4528a
exuszee <exuszee@gmail.com>
2023-05-18 16:53:21 +0300

    1-fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def index():
     """
@@ -12,12 +13,14 @@ def index():
     """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb")
 def display_HBNB():
     """
     Route to display “HBNB!”
     """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
