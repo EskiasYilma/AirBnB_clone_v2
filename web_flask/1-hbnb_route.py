f7f2106364965994171f40c520188abeaf952d9c
exuszee <exuszee@gmail.com>
2023-05-18 16:48:26 +0300

    1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def index():
+    """
+    Route to display “Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def display_HBNB():
+    """
+    Route to display “HBNB!”
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
