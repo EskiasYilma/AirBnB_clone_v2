0a0e9def2ed3d0f55c08d4ed1b8d7b250c7e5c41
exuszee <exuszee@gmail.com>
2023-05-19 18:52:17 +0300

    8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def states_list():
+    """
+    Route to display a HTML page
+     (inside the tag BODY)
+        H1 tag: “States”
+        UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
+            LI tag: description of one State: <state.id>: <B><state.name></B>
+    """
+    all_states = storage.all(State)
+    for state_id, state in all_states.items():
+        for city in state.cities:
+            print("Find the city {} in the state {}".format(city, state))
+
+    return render_template("7-states_list.html")
+
+
+@app.teardown_appcontext
+def teardown():
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
